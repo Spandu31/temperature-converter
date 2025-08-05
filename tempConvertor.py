a6d2332f22cc50a06b34bd70a41fb16dc8343517
Spandu31 <146758331+Spandu31@users.noreply.github.com>
2025-08-05 23:52:23 +0530

    Update tempConvertor.py

--- tempConvertor.py
+++ tempConvertor.py
@@ -3,18 +3,19 @@ print("Enter the temperature:")
 num = float(input())
 
 print("Enter 'C' to convert from Celsius to Fahrenheit or 'F' to convert from Fahrenheit to Celsius:")
-deg = input()
+unit = input()
 
 
-print("You entered:", num, deg)
+print("You entered:", num, unit)
 
-def calc(num, deg):
-    if deg == "c" or deg == "C":
+def calc(num, unit):
+    if unit == "c" or unit == "C":
         temp = (num * 9/5) + 32
         print(f"Temperature in Fahrenheit:", f"{temp:.2f}", "F")
-    elif deg == "f" or deg == "F":
+    elif unit == "f" or unit == "F":
         temp = (num - 32) * 5/9
         print("Temperature in Celsius:", f"{temp:.2f}", "C")
     else:
         print("Invalid unit. Please enter 'C' or 'F'.")
-calc(num, deg)
+calc(num, unit)
+
